36007ccd77484c9ca5b1ef3c9f74432eca3617e2
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-07 23:01:49 -0600

    initial commit/notes

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*
+Project pivot
+Step 1: make a successful call from both API's
+Step 2: create general HTML setup
+    - title
+    - div for the youtube video?
+    - add user input to pick a task "add up to 4 tasks"
+    - start button for timer
+Step 3: 
+    - when the user clicks start, 
+        - the timer is displayed under the app title
+        - an inspirational quote is displayed under the title
+    - the user's first task is displayed to them
+    - chillhop study girl youtube video is displayed and plays
+Step 4: 
+    - create the timer function to countdown from 25 minutes
+        - use secconds for development
+Step 5: when the timer runs out, display a photo from the unsplash api
+    - another timer starts for 5 minutes
+Step 6: figure out how to loop the timer so that with each loop, the next task is displayed
+Step 7: after the fourth round, display "time to take a longer break: 15-30 minutes"
+    - "restart the app after your break to continue"...or something like that
+*/
